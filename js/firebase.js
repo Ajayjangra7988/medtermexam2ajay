13e91b27619aae9094e65d2a8e89678495954ce1
Ajayjangra7988 <a34ajay@gmail.com>
2023-12-01 16:54:56 -0800

    new js folder

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfn9mhCytHm21bCvcBLEj7CpN4W0pFOC0",
+    authDomain: "cctb-sign-in-project.firebaseapp.com",
+    projectId: "cctb-sign-in-project",
+    storageBucket: "cctb-sign-in-project.appspot.com",
+    messagingSenderId: "34340817027",
+    appId: "1:34340817027:web:82a9d4c4dfb97c1d1173b6"
+  };
+
+  firebase.initializeApp(firebaseConfig);
